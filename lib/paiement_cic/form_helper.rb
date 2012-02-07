e267828e17df09114dda156738f71cb54dce6253
Benoit Garret <benoit.garret@gadz.org>
2012-02-07 11:39:20 +0100

    Uncouple the form helper from the implementation details

--- lib/paiement_cic/form_helper.rb
+++ lib/paiement_cic/form_helper.rb
@@ -1,16 +1,12 @@
 ## refactor this
-module PaiementCic::FormHelper
-  def paiement_cic_hidden_fields(order, price, order_transaction, options = {})
-    oa = PaiementCic.config(price, order_transaction.reference)
+module PaiementCicHelper
+  def paiement_cic_hidden_fields(price, order_reference, options = {})
+    oa = PaiementCic.config(price, order_reference)
 
     oMac = PaiementCic.new
     sDate = Time.now.strftime("%d/%m/%Y:%H:%M:%S")
     chaine = [oMac.tpe, sDate, oa["montant"], oa["reference"].to_s, oa["texte-libre"], oMac.version, "FR", oMac.societe, "", "", "", "", "", "", "", "", "", "", ""].join("*")
     chaineMAC = oMac.computeHMACSHA1(chaine)
-    
-    url_retour      = options[:url_retour] || bank_callback_order_transactions_url
-    url_retour_ok   = options[:url_retour_ok] || bank_callback_order_transactions_url(order)
-    url_retour_err  = options[:url_retour_err] || bank_err_order_transaction_url(order)
     
     html = '
         <input type="hidden" name="version"           id="version"        value="' + oa["version"] + '" />
@@ -19,9 +15,9 @@ module PaiementCic::FormHelper
         <input type="hidden" name="montant"           id="montant"        value="' + oa["montant"] + '" />
         <input type="hidden" name="reference"         id="reference"      value="' + oa["reference"].to_s + '" />
         <input type="hidden" name="MAC"               id="MAC"            value="' + chaineMAC + '" />
-        <input type="hidden" name="url_retour"        id="url_retour"     value="' + url_retour + '" />
-        <input type="hidden" name="url_retour_ok"     id="url_retour_ok"  value="' + url_retour_ok + '" />
-        <input type="hidden" name="url_retour_err"    id="url_retour_err" value="' + url_retour_err + '" />
+        <input type="hidden" name="url_retour"        id="url_retour"     value="' + options[:url_retour] + '" />
+        <input type="hidden" name="url_retour_ok"     id="url_retour_ok"  value="' + options[:url_retour_ok] + '" />
+        <input type="hidden" name="url_retour_err"    id="url_retour_err" value="' + options[:url_retour_err] + '" />
         <input type="hidden" name="lgue"              id="lgue"           value="' + oa["lgue"] + '" />
         <input type="hidden" name="societe"           id="societe"        value="' + oa["societe"] + '" />
         <input type="hidden" name="texte-libre"       id="texte-libre"    value="' + oa["texte-libre"] + '" />
